cd830ee8213d970dd67ed2de2d7197428f3f7d9e
GustavoCR021 <gustavoc1922@gmail.com>
2024-04-25 00:11:26 -0300

    at/f

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 var password = document.getElementById("password");
 function generate() {
-  var char = ""
+  var char = "1234567890QWERTYUIOPASDFGHJKLXCVBNM"
+  var password = "";
+  var Length = 10;
+  for (var i = 0; 1 <= Length; i++) {
+    var randonNum = Math.floor(Math.random() * char.length);
+    password += char.substring(randonNum, randonNum + 1);
+  }
+  document.getElementById("password").value = password;
 }
